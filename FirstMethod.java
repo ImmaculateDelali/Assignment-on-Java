8a4c63c027d6daebb80d89432b2374d1b684ea05
Immaculate Delali <florenceahorlu@meltwater.org>
2022-10-15 19:38:31 +0000

    new file

---
+++ FirstMethod.java
@@ -0,0 +1,19 @@
+public void FirstMethod() {
+    int DayoftheWeek = 1;
+        String day = "";
+        switch (DayoftheWeek) {
+            case 1:
+                day ="Sunday";
+                break;
+
+            case 2:
+                day ="Monday";
+
+            case 3:
+                day = "Tuesday";
+                 
+            default:
+            System.out.println(day);
+                break;
+        }
+}
--- TryingMethods.java
+++ TryingMethods.java
@@ -5,44 +5,12 @@ import java.time.DayOfWeek;
 public class TryingMethods {
     // main method
     public static void main(String[] args) {
-        FourthMethod teste = new FourthMethod();
         
     }
-// static method with a switch statement
-  /*   public static void FirstMethod() {
-        int DayoftheWeek = 1;
-        String day = "";
-        switch (DayoftheWeek) {
-            case 1:
-                day ="Sunday";
-                break;
 
-            case 2:
-                day ="Monday";
-
-            case 3:
-                day = "Tuesday";
-                 
-            default:
-            System.out.println(day);
-                break;
-        }
-    }
-    */
-// public method with an if statement
-   /*  public static void SecondMethod() {
-        int examScore = 100;
-        if (marks == 80) {
-            System.out.println("You had an A");
-        } 
-    }
-    */
-
-//dynamic method with a while loop to find the sum of numbers below 1000
-   // public void ThirdMethod() {
-        
-    //}
-    //dynamic method with a while loop to find the sum of even numbers between 12 and 103
+    //dynamic method with a while loop to find the sum of numbers below 1000
+   
+    /* dynamic method with a while loop to find the sum of even numbers between 12 and 103
     public void FourthMethod() {
         int range = 12;
         while (range <= 103){
@@ -51,5 +19,19 @@ public class TryingMethods {
         }
     }
     
+// static method with a switch statement
+    public static void FirstMethod() {
+        
+    }
+    
+// public method with an if statement
+    public static void SecondMethod() {
+        int examScore = 100;
+        
+        } 
+    }
+    
+*/
+
 
 }

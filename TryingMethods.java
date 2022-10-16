ce9e0cb852ceb2883821dc1d45be488f108f64a1
Immaculate Delali <florenceahorlu@meltwater.org>
2022-10-16 11:24:19 +0000

    Updates

--- TryingMethods.java
+++ TryingMethods.java
@@ -6,34 +6,8 @@ public class TryingMethods {
     public static void main(String[] args) {
         //FirstMethod.anothermethod();
         //SecondMethod.BlocksOfif();
-        //ThirdMethod.Whilesample();
+        ThirdMethod.Whilesample();
         FourthMethod.EvenNumbers();
     }
-
-    //dynamic method with a while loop to find the sum of numbers below 1000
-   
-    /* dynamic method with a while loop to find the sum of even numbers between 12 and 103
-    public void FourthMethod() {
-        int range = 12;
-        while (range <= 103){
-            System.out.println("sum" + range);
-            range +=range;
-        }
-    }
-    
-// static method with a switch statement
-    public static void FirstMethod() {
-        
-    }
-    
-// public method with an if statement
-    public static void SecondMethod() {
-        int examScore = 100;
-        
-        } 
-    }
-    
-*/
-
 
 }
